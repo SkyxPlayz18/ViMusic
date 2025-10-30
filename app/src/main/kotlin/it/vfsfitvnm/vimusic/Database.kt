8245510aba908bae7411f6886876cb2ab404bab7
SkyxPlayz18 <rehanmaulana7373@gmail.com>
2025-10-30 12:31:30 +0700

    Update Database.kt

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
@@ -1016,7 +1016,6 @@ fun insertPreserve(mediaItem: MediaItem, block: (Song) -> Song = { it }) {
         AutoMigration(from = 27, to = 28),
         AutoMigration(from = 28, to = 29),
         AutoMigration(from = 29, to = 30),
-        AutoMigration(from = 33, to = 34, spec = DatabaseInitializer.From33To34Migration::class)
     ]
 )
 @TypeConverters(Converters::class)
