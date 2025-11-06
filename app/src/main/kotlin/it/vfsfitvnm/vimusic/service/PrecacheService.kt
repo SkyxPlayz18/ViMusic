81cf0df61ad512b8c2a7d4ca9571b92f4af6cfad
SkyxPlayz18 <rehanmaulana7373@gmail.com>
2025-11-06 21:16:28 +0700

    Update PrecacheService.kt

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/service/PrecacheService.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/service/PrecacheService.kt
@@ -44,6 +44,7 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.firstOrNull
 import java.io.File
 import java.util.concurrent.Executors
 import kotlin.coroutines.resume
