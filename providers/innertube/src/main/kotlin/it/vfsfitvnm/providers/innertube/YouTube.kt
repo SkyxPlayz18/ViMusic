1b58121345382bff60af46c3cbb48fed7c909737
SkyxPlayz18 <rehanmaulana7373@gmail.com>
2026-02-01 12:09:58 +0700

    Update YouTube.kt

--- providers/innertube/src/main/kotlin/it/vfsfitvnm/providers/innertube/YouTube.kt
+++ providers/innertube/src/main/kotlin/it/vfsfitvnm/providers/innertube/YouTube.kt
@@ -1,10 +1,13 @@
 package it.vfsfitvnm.providers.innertube
 
 import java.net.Proxy
-object YouTube{
-    var proxy: Proxy?
-        get() = Innertube.proxy
+
+object YouTube {
+    var visitorData: String = "CgtXczFGNjhHaGsyOCi9i7W0BjIKCgJJRBIEGgAgOw%3D%3D"
+    
+    var proxy: Proxy? = null
         set(value) {
+            field = value
             Innertube.proxy = value
         }
 }
